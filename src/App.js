9f68548ccfe131c25e69c729ad3d40ade34636eb
Edwin Lin <edwlin7279@gmail.com>
2025-07-09 20:37:03 -0700

    Add progress: Pocket Racers and Digital Audio VIsualizer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 
 import IEEElogo from './IEEE_logo.jpeg';
-import ahh from './mountains.jpeg';
 import robot from './3d_animated.gif';
 import microm from './micromouse.png';
+import pocket from './pocketracers.png';
+import dav from './dav.gif';
 import './App.css';
 
 
@@ -48,16 +49,20 @@ function App() {
             </p>
 
           <h3> Pocket Racers </h3>
-
-            {/* edwin */}
-
+            <img src={pocket} alt="PocketRacer" className="pocketracer-image" />
+            <p className="body-text"> Students work together to build an autonomous car that uses computer vision and machine learning to navigate a track on its own. 
+They start by driving the car manually to collect training data, then train a neural network that enables the car to drive itself.
+Throughout the school year, students develop the car’s hardware and software, applying what they learn to optimize performance for the final demonstration at the end of spring quarter.
+Students often walk away with valuable hands-on experience in Raspberry Pi development, computer vision with OpenCV, data preprocessing, and building and training convolutional neural networks, making them well-prepared for careers in embedded systems and machine learning.
+            </p>
 
           <h3> Digital Audio Visualizer</h3>
-
-
-            {/* edwin */}
-          
-        
+            <img src={dav} alt="dav" className="digitalaudiovisualizer-image" />
+            <p className="body-text"> Students work together to design, build, and test digital logic systems while gaining hands-on experience with real hardware.
+They choose between two exciting tracks: one focused on creating a digital audio visualizer that displays frequency levels on a VGA screen, and another focused on designing and verifying a digital game that interacts with external peripherals.
+Throughout the school year, students develop a strong foundation in sequential logic, communication protocols, and graphics controllers, culminating in a capstone project that showcases their work.
+Students often walk away with practical experience in digital design, verification techniques, and industry-standard practices, making them strong candidates for roles in hardware design and verification engineering.
+            </p>
 
 
         <a
